ba88a05eb04feabda26984c477a406102580cf50
Kirthi Prakash <kirthiprakash.r@gmail.com>
2021-05-07 16:49:46 +0530

    commit message: Fri May  7 16:49:46 IST 2021

--- bulk_commit.sh
+++ bulk_commit.sh
@@ -4,6 +4,7 @@ for i in {1..10}
 do
   date=$(exec date)
   echo "$1 $date" > app/file.txt
-  git commit -am "commit message: $date"
+  git add -u
+  git commit -m "commit message: $date"
 done
 git push origin master
